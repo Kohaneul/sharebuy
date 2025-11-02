adde0ced9707c3cdf9c1fcc2c58331c23b6280a1
hanul1106 <151487414+hanul1106@users.noreply.github.com>
2025-11-02 20:44:42 +0900

    Initial commit

---
+++ src/main/java/com/project/sharebuy/SharebuyApplication.java
@@ -0,0 +1,13 @@
+package com.project.sharebuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharebuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharebuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/sharebuy/SharebuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sharebuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharebuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
