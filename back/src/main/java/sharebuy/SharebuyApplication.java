abaeb906eced30065af8ebd5d4c32b4df3b383e8
hanul1106 <151487414+hanul1106@users.noreply.github.com>
2025-11-09 21:19:43 +0900

    1109 commit

--- back/src/main/java/sharebuy/SharebuyApplication.java
+++ back/src/main/java/sharebuy/SharebuyApplication.java
@@ -1,13 +1,11 @@
-package com.project.sharebuy;
+package sharebuy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SharebuyApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SharebuyApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SharebuyApplication.class, args);
+    }
 }
